e6c2897369b7a739685aea38b16097afacfcfe15
Alba Pérez César <alba.perez.ce@gmail.com>
2021-03-02 18:29:04 +0100

    commit final

---
+++ Movies_MVC_ASP.NET_Core/Data/AdventureWorksLT2017Context.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies_MVC_ASP.NET_Core.Models;
+
+namespace Movies_MVC_ASP.NET_Core.Data
+{
+    public partial class AdventureWorksLT2017Context : DbContext
+    {
+        public AdventureWorksLT2017Context()
+        {
+        }
+
+        public AdventureWorksLT2017Context(DbContextOptions<AdventureWorksLT2017Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movies> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Initial Catalog=AdventureWorksLT2017;Data Source=PLX00135100911\\SQLEXPRESS;Integrated Security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<ErrorLog>(entity =>
+            {
+                entity.Property(e => e.ErrorLogId).HasColumnName("ErrorLogID");
+
+                entity.Property(e => e.ErrorMessage)
+                    .IsRequired()
+                    .HasMaxLength(4000);
+
+                entity.Property(e => e.ErrorProcedure).HasMaxLength(126);
+
+                entity.Property(e => e.ErrorTime)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Movies>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Genre)
+                    .HasColumnName("genre")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price)
+                    .HasColumnName("price")
+                    .HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ReleaseDate)
+                    .HasColumnName("release_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Title)
+                    .HasColumnName("title")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
